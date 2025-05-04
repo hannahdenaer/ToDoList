ec7bd220f942ff41a514ba1e7dfd0421dfa81b4d
Hannah Denaer <denaerhannah@gmail.com>
2025-05-04 10:06:11 -0500

    Set up Content View UI

---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by Scholar on 5/4/25.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
