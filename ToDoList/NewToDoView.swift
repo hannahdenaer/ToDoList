c833ad44c6bb5cfd65e71bc87b943cebf3d8a6ed
Hannah Denaer <denaerhannah@gmail.com>
2025-05-04 10:22:13 -0500

    Added NewToDoView

--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task Title:")
+                .font(.title)
+                .fontWeight(.bold)
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding ()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding ()
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+                    .padding ()
+                
+                Button {
+                    
+                } label: {
+                    Text("Save")
+                .fontWeight(.bold)
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                }
+            }
+        }
+        .padding ()
     }
 }
 
